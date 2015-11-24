044ce59957852c8abc449d305e77b08c302bdba7
chadqueen <chaddolan82@gmail.com>
2015-11-24 16:16:21 -0700

    Add player and game actors and messages

---
+++ Game.ActorModel/Actors/GameControllerActor.cs
@@ -0,0 +1,51 @@
+﻿using Akka.Actor;
+using Game.ActorModel.Messages;
+using System.Collections.Generic;
+
+namespace Game.ActorModel.Actors
+{
+    public class GameControllerActor : ReceiveActor
+    {
+        private readonly Dictionary<string, IActorRef> _players;
+
+        public GameControllerActor()
+        {
+            this._players = new Dictionary<string, IActorRef>();
+
+            Receive<JoinGameMessage>(
+                message =>
+                {
+                    this.JoinGame(message);
+                }
+            );
+
+            Receive<AttackPlayerMessage>(
+                message =>
+                {
+                    this._players[message.PlayerName].Forward(message);
+                }
+            );
+        }
+
+        private void JoinGame(JoinGameMessage message)
+        {
+            var playerNeedsCreating = !this._players.ContainsKey(message.PlayerName);
+
+            if (playerNeedsCreating)
+            {
+                IActorRef newPlayerActor =
+                    Context.ActorOf(
+                        Props.Create(() => new PlayorActor(message.PlayerName, 100)),
+                        message.PlayerName
+                    );
+
+                this._players.Add(message.PlayerName, newPlayerActor);
+
+                foreach (var player in this._players.Values)
+                {
+                    player.Tell(new RefreshPlayerStatusMessage(), Sender);
+                }
+            }
+        }
+    }
+}
---
+++ Game.ActorModel/Actors/PlayorActor.cs
@@ -0,0 +1,34 @@
+﻿using Akka.Actor;
+using Game.ActorModel.Messages;
+
+namespace Game.ActorModel.Actors
+{
+    public class PlayorActor: ReceiveActor
+    {
+        private readonly string _playerName;
+
+        private int _health;
+
+        public PlayorActor(string playerName, int health)
+        {
+            this._playerName = playerName;
+            this._health = health;
+
+            Receive<AttackPlayerMessage>(
+                message =>
+                {
+                    this._health -= 20;
+
+                    Sender.Tell(new PlayerHealthChangedMessage(this._playerName, this._health));
+                }    
+            );
+
+            Receive<RefreshPlayerStatusMessage>(
+                message =>
+                {
+                    Sender.Tell(new PlayerStatusMessage(this._playerName, this._health));
+                }
+            );
+        }
+    }
+}
